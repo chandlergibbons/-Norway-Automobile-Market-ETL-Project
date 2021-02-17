a1105330d7d7bd990fffd966905485e00d413b04
Chandler Gibbons <chandlergibbons6@gmail.com>
2021-02-16 20:00:11 -0500

    first draft of Chandlers end of the ETL

---
+++ table_create_query.sql
@@ -0,0 +1,17 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/A9MeKz
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "sales_df_yr" (
+    "Year" int   NOT NULL,
+    "Model" VARCHAR   NOT NULL,
+    "Make" VARCHAR   NOT NULL,
+    "Quantity" int   NOT NULL,
+    "Sales_Rating" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_sales_df_yr" PRIMARY KEY (
+        "Year","Model"
+     )
+);
+
+
