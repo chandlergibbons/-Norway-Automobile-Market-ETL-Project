311e573dcf5eda0757d528a11a13401561420c6f
Chandler Gibbons <chandlergibbons6@gmail.com>
2021-02-16 20:25:48 -0500

    update create table query

--- table_create_query.sql
+++ table_create_query.sql
@@ -1,9 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/A9MeKz
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "sales_df_yr" (
+﻿CREATE TABLE "sales_df_yr" (
     "Year" int   NOT NULL,
     "Model" VARCHAR   NOT NULL,
     "Make" VARCHAR   NOT NULL,
@@ -13,5 +8,23 @@ CREATE TABLE "sales_df_yr" (
         "Year","Model"
      )
 );
+
+CREATE TABLE "fueleconomy" (
+    "ID" int   NOT NULL,
+    "Make" VARCHAR   NOT NULL,
+    "Model" VARCHAR   NOT NULL,
+    "Year" bigint   NOT NULL,
+    "Vehicle_Class" VARCHAR   NOT NULL,
+    "Drive" VARCHAR   NOT NULL,
+    "Fuel_Type" VARCHAR   NOT NULL,
+    "Fuel_Economy_Score" bigint   NOT NULL,
+    "Good_Fuel_Economy?" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_fueleconomy" PRIMARY KEY (
+        "Model","Year"
+     )
+);
+
+ALTER TABLE "fueleconomy" ADD CONSTRAINT "fk_fueleconomy_Model_Year" FOREIGN KEY("Model", "Year")
+REFERENCES "sales_df_yr" ("Model", "Year");
 
 
